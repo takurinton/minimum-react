1dd559e46d4141bf9e36f99023656c46f01a6fd9
takurinton <taryochanman@icloud.com>
2021-02-04 02:05:33 +0900

    fix

--- src/hooks/useForm.ts
+++ src/hooks/useForm.ts
@@ -1,24 +1,25 @@
 import { useState } from 'react';
 
-interface ContactProps {
+interface Props {
     name: string, 
     age: number, 
 };
-const initialContactState: ContactProps =  {
+const initialState: Props =  {
     name: '', 
     age: 0, 
 };
 
 export const useForm = () => {
-    const [state, setState] = useState(initialContactState);
+    const [state, setState] = useState(initialState);
 
     const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
         setState({...state, [e.target.name]: e.target.value})
     };
     
-    const handleSubmit = (body:object) => {
-        console.log(JSON.stringify(body))
-        setState(initialContactState)
+    const handleSubmit = (body:Props) => {
+        const content = `${body.name}さん，${body.age}才`;
+        alert(content);
+        setState(initialState)
     };
 
     return {
